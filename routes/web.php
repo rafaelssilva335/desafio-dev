c82ddd84b7ae17cd202eb4bc183a78ce97333547
Rafael Silva e Silva <rafaelssilva35@gmail.com>
2022-03-22 16:23:22 -0300

    Simple form upload file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UploadCNABFile;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/upload/CNAB', UploadCNABFile::class);
